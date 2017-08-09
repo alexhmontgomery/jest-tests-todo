93e743bd25711e5055ff4505a4f15b7a2c13a88b
Alex <alexhmontgomery@gmail.com>
2017-08-09 15:52:44 -0500

    complete all remaining tests, including action tests

--- src/actions.test.js
+++ src/actions.test.js
@@ -1,1 +1,29 @@
-// write at least 1 action test
+import { createTodo, filterTodos, toggleTodo } from './actions'
+import { CREATE_TODO, FILTER_TODOS, TOGGLE_TODO } from './actions'
+
+describe('actions', () => {
+  it('creates action to add todo', () => {
+    const expectedAction = {
+      type: CREATE_TODO
+    }
+    expect(createTodo()).toEqual(expectedAction)
+  })
+})
+
+describe('actions', () => {
+  it('creates action to toggles the status of done between true and false', () => {
+    const expectedAction = {
+      type: TOGGLE_TODO
+    }
+    expect(toggleTodo()).toEqual(expectedAction)
+  })
+})
+
+describe('actions', () => {
+  it('creates an action to filter between completed and incomplete todos', () => {
+    const expectedAction = {
+      type: FILTER_TODOS
+    }
+    expect(filterTodos()).toEqual(expectedAction)
+  })
+})
